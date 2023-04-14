aa8c257135d801dab99d96f44594331dcfc571ef
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-14 11:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function threeSum(arr, target) {
+function decimalToBinary(n) {
 // write your code here
-  
+	arr = [];
+	while(target>1){
+		arr.push(target%2);
+		target = Math.floor(target/2);
+	}
+	arr.push(1);
+	return arr.reverse().join("");
 }
 
 module.exports = threeSum;
